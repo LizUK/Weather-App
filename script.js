080fc912dde310571faa5a4adcf0e3802f760e90
Elizabeth Phillips <19333389+LizUK@users.noreply.github.com>
2025-09-16 20:20:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function getWeather(e) {
     const apiKey = "0ee82ee6fda955a1527443dccdcd8c59";
     
     console.log(keyword);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${keyword}&appid=${apiKey}&units=metric``,  { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${keyword}&appid=${apiKey}&units=metric``),  { mode: 'cors' });
     const weatherData = await response.json();
     console.log(weatherData);
 
@@ -71,6 +71,9 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     getWeather(e);
 });
+
+console.log("CHANGED");
+
 
 
 
